1df810f072726acdc5e15943f13a9c6ec8cc2922
shahdsamara <sh.samara94@gmail.com>
2024-09-14 15:48:13 -0700

    Update Lake_Tulloch_Flood_Control_Requirement.py

--- sierra/models/stanislaus/_parameters/Lake_Tulloch_Flood_Control_Requirement.py
+++ sierra/models/stanislaus/_parameters/Lake_Tulloch_Flood_Control_Requirement.py
@@ -1,6 +1,6 @@
-from sierra.base_parameters import BaseParameter
+from cen_sierra_pywr.sierra.base_parameters import BaseParameter
 
-from sierra.utilities.converter import convert
+from cen_sierra_pywr.sierra.utilities.converter import convert
 
 
 class Lake_Tulloch_Flood_Control_Requirement(BaseParameter):
