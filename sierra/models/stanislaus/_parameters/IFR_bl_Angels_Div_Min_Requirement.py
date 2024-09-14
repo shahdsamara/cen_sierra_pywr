7cb5f58319a78ff1f1983ba7566d50efc3f27dcd
shahdsamara <sh.samara94@gmail.com>
2024-09-14 15:32:52 -0700

    Update IFR_bl_Angels_Div_Min_Requirement.py

--- sierra/models/stanislaus/_parameters/IFR_bl_Angels_Div_Min_Requirement.py
+++ sierra/models/stanislaus/_parameters/IFR_bl_Angels_Div_Min_Requirement.py
@@ -1,6 +1,6 @@
-from sierra.base_parameters import MinFlowParameter
+from cen_sierra_pywr.sierra.base_parameters import MinFlowParameter
 
-from sierra.utilities.converter import convert
+from cen_sierra_pywr.sierra.utilities.converter import convert
 
 
 class IFR_bl_Angels_Div_Min_Requirement(MinFlowParameter):
