9a3aae7e6c2bd2ce5a2c07f508bbb89e556ce06c
shahdsamara <sh.samara94@gmail.com>
2024-09-14 15:31:48 -0700

    Update Donnells_PH_Turbine_Capacity.py

--- sierra/models/stanislaus/_parameters/Donnells_PH_Turbine_Capacity.py
+++ sierra/models/stanislaus/_parameters/Donnells_PH_Turbine_Capacity.py
@@ -1,6 +1,6 @@
-from sierra.base_parameters import BaseParameter
+from cen_sierra_pywr.sierra.base_parameters import BaseParameter
 
-from sierra.utilities.converter import convert
+from cen_sierra_pywr.sierra.utilities.converter import convert
 
 
 class Donnells_PH_Turbine_Capacity(BaseParameter):
