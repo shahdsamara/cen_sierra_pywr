4951440d31c1cda66f4f34783206d21caaf2f6c3
shahdsamara <sh.samara94@gmail.com>
2024-09-14 15:59:47 -0700

    Update Upper_Collierville_Tunnel_1_Capacity.py

--- sierra/models/stanislaus/_parameters/Upper_Collierville_Tunnel_1_Capacity.py
+++ sierra/models/stanislaus/_parameters/Upper_Collierville_Tunnel_1_Capacity.py
@@ -1,6 +1,6 @@
-from sierra.base_parameters import BaseParameter
+from cen_sierra_pywr.sierra.base_parameters import BaseParameter
 
-from sierra.utilities.converter import convert
+from cen_sierra_pywr.sierra.utilities.converter import convert
 
 
 class Upper_Collierville_Tunnel_1_Capacity(BaseParameter):
