48947ff2dd6729ba8c99c9410cc59039025b23ee
shahdsamara <sh.samara94@gmail.com>
2024-09-14 15:31:27 -0700

    Update Donnell_Lake_Spill_Min_Requirement.py

--- sierra/models/stanislaus/_parameters/Donnell_Lake_Spill_Min_Requirement.py
+++ sierra/models/stanislaus/_parameters/Donnell_Lake_Spill_Min_Requirement.py
@@ -1,6 +1,6 @@
-from sierra.base_parameters import MinFlowParameter
+from cen_sierra_pywr.sierra.base_parameters import MinFlowParameter
 
-from sierra.utilities.converter import convert
+from cen_sierra_pywr.sierra.utilities.converter import convert
 
 
 class Donnell_Lake_Spill_Min_Requirement(MinFlowParameter):
